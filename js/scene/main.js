bde89abec964abf42fa0833e96aecae4beb5f4b8
Game-Awake <projeto.game.awake@gmail.com>
2021-12-18 21:01:02 -0300

    Fix

--- js/scene/main.js
+++ js/scene/main.js
@@ -5,6 +5,7 @@ class Main extends Phaser.Scene
     {
         super('main');
         this.board = {teams:[],questions:[]};
+        this.step = 70;
     }
 
     init(data) {
@@ -33,7 +34,7 @@ class Main extends Phaser.Scene
         wordWrap: { width: 100, useAdvancedWrap: true }
       }
 
-      let next = this.add.text(10,10,"Próxima",style);
+      let next = this.add.text(this.step+10,10,"Próxima",style);
       next.setPadding(16,16);
       next.setInteractive();
       next.on("pointerdown",() => {
@@ -54,10 +55,10 @@ class Main extends Phaser.Scene
           wordWrap: { width: 100, useAdvancedWrap: true }
         }
         let text = "Team " + id;
-        this.board.teams[id].name = this.add.text(80,120+i*40,text,style);
+        this.board.teams[id].name = this.add.text(this.step+80,120+i*40,text,style);
         this.board.teams[id].name.setOrigin(0.5);
         this.board.teams[id].name.setInteractive();
-        this.board.teams[id].score = this.add.text(80,120+(i+1)*40,"0",style);
+        this.board.teams[id].score = this.add.text(this.step+80,120+(i+1)*40,"0",style);
         this.board.teams[id].score.setOrigin(0.5);
         this.board.teams[id].points = 0;
       }
@@ -70,7 +71,7 @@ class Main extends Phaser.Scene
         wordWrap: { width: 800, useAdvancedWrap: true }
       }
 
-      this.questionLabel = this.add.text(150,10,"",style);
+      this.questionLabel = this.add.text(this.step+150,10,"",style);
       this.board.answers = [];
 
       this.showQuestion();
@@ -100,11 +101,11 @@ class Main extends Phaser.Scene
       for(let j in this.question.answers) {
         this.board.answers[i] = {value:j,percentage:this.question.answers[j]};
         text = "";
-        this.board.answers[i].optionValue = new Option(300+x*350,150+y*100,150,80,
+        this.board.answers[i].optionValue = new Option(this.step+300+x*350,150+y*100,150,80,
           0x0000ff,2,0xffffff,
           text, style,
           () => {}, this);
-        this.board.answers[i].optionPercentage = new Option(450+x*350,150+y*100,100,80,
+        this.board.answers[i].optionPercentage = new Option(this.step+450+x*350,150+y*100,100,80,
           0x0000ff,2,0xffffff,
           text, style,
           () => {}, this);
@@ -116,7 +117,7 @@ class Main extends Phaser.Scene
         i++;
       }
 
-      this.element = this.add.dom(100, 200).createFromCache("input");
+      this.element = this.add.dom(this.step, 200).createFromCache("input");
       this.element.addListener("click");
       this.element.on("click", (event) => {
           if (event.target.name === "playButton") {
